92b01087fdf18b1a31fb0d6e09ce1f44b27cf404
alois9866 <alois9866@gmail.com>
2018-02-16 13:49:54 +0300

    Create gloss application stub

--- src/Straminata.hs
+++ src/Straminata.hs
@@ -1,4 +1,18 @@
 module Straminata where
 
+import Graphics.Gloss
+
+data Game = Game
+
 run :: IO ()
-run = putStrLn "This project is not yet implemented"
+run = play
+      (InWindow "straminata project" (800, 600) (10, 10))
+      white
+      30
+      Game
+      (const picture)
+      (\_ _ -> Game)
+      (\_ _ -> Game)
+
+picture :: Picture
+picture = Translate (-170) 0 $ Scale 0.5 0.5 $ Text "Hello World"
