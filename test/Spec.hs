24a6096a846041ae906a7b2350b20aadfa59c519
Chocolate queen <NUT1997020501@yandex.ru>
2018-02-21 00:34:52 +0300

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,10 +6,10 @@ import Util.Common
 
 main :: IO ()
 main = hspec $ do
-  let h1 = Rectangle (Position (0, 0), Position (2, 2))
-      h2 = Rectangle (Position (1, 1), Position (3, 3))
-      h3 = Rectangle (Position (3, 0), Position (5, 2))
-      h4 = Rectangle (Position (-1, -1), Position (1, 1))
+  let h1 = (Position (0, 0), Position (2, 2))
+      h2 = (Position (1, 1), Position (3, 3))
+      h3 = (Position (3, 0), Position (5, 2))
+      h4 = (Position (-1, -1), Position (1, 1))
   describe "Prelude.head" $ do
     it "checks if rectangles collide" $ do
       collide h1 h2 `shouldBe` True
