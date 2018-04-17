2871ca5c4f4536b986cbe6ff327411bb30655c18
alois9866 <alois9866@gmail.com>
2018-04-17 15:57:15 +0300

    WIP

--- src/Generator.hs
+++ src/Generator.hs
@@ -3,39 +3,91 @@ module Generator where
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 
+import Control.Monad (join)
+import Control.Arrow ((***))
+import Debug.Trace(trace)
+
 import Model.CommonTypes
+import Visual.Renderer
+import Visual.TextureLoader
 import Visual.WindowConstants
 
+-- | Map generator
 data Generator = Generator
   { generatorButtons :: [[Button]]
   }
 
-data ButtonState = None | Grass | Dirt | Coin -- | Button | Door
-  deriving (Enum)
+initialGeneratorState :: Generator
+initialGeneratorState = Generator
+  { generatorButtons = generateEmptyButtons
+  }
 
-nextState :: ButtonState -> ButtonState
-nextState Coin = None
-nextState s = succ s
-
-stateColor :: ButtonState -> Color
-stateColor None = white
-stateColor Grass = green
-stateColor Dirt = black
-stateColor Coin = yellow
-
+-- | Button that represents a map tile
 data Button = Button
   { buttonPos :: Position
   , buttonState :: ButtonState
+  , buttonNum :: Int
   }
 
-maximumMapSize :: Float
-maximumMapSize = 30
+-- | Map tile type
+data ButtonState = None | Grass | Dirt | Coin | ButtonObject | Door | Exit1 | Exit2 | Player1 | Player2
+  deriving (Eq, Enum, Bounded)
+
+-- | Get next tile variant
+nextState :: ButtonState -> ButtonState
+nextState s = if s == (maxBound :: ButtonState) then minBound else succ s
+
+-- | Get map tile appearance
+buttonAppearance :: ButtonState -> Position -> Appearance
+buttonAppearance None = createAppearance transparentTexture
+buttonAppearance Grass = createAppearance dirtTextureWithGrass
+buttonAppearance Dirt = createAppearance dirtTextureWithoutGrass
+buttonAppearance Coin = createAppearance coinTexture
+buttonAppearance ButtonObject = createAppearance buttonTexture
+buttonAppearance Door = createAppearance doorCloseTexture
+buttonAppearance Exit1 = createAppearance teleport1Texture
+buttonAppearance Exit2 = createAppearance teleport2Texture
+buttonAppearance Player1 = createAppearance exaIdle1
+buttonAppearance Player2 = createAppearance ineIdle1
+
+-- | Default appearance template
+createAppearance :: Texture -> Position -> Appearance
+createAppearance texture (Position (x, y)) = Appearance
+  { appearanceBox = ( Position (x * buttonLinearSize, y * buttonLinearSize)
+                    , Position ((x + 1) * buttonLinearSize, (y + 1) * buttonLinearSize))
+  , appearanceActualSize = fst texture
+  , appearanceAnimation = [snd texture]
+  }
+
+nextNum :: Int -> Int
+nextNum 9 = 0
+nextNum n = n + 1
+
+
+-- | UI button
+data SystemButton = SystemButton
+  { systemButtonPosition :: Position
+  , systemButtonText :: String
+  }
+
+systemButtons :: [SystemButton]
+systemButtons = generateSystemButtons
+
+generateSystemButtons :: [SystemButton]
+generateSystemButtons =
+  [ SystemButton
+      { systemButtonPosition = Position $ join (***) ((\x -> x - 200) . (/2) . fromIntegral) initialWindowDimensions
+      , systemButtonText = "Generate"
+      }
+  ]
+
+
 
 generateEmptyButtons :: [[Button]]
 generateEmptyButtons = map (scaleRow buttonLinearSize) rowSetter
   where
     rowSetter = createWithNumber maximumMapSize (\r n -> map (setterY n) r) row
-    row = createWithNumber maximumMapSize setterX $ Button { buttonPos = Position (0, 0), buttonState = None }
+    row = createWithNumber maximumMapSize setterX $ Button { buttonPos = Position (0, 0), buttonState = None, buttonNum = 0 }
     setterX button x = button { buttonPos = setX x (buttonPos button) }
     setterY y button = button { buttonPos = setY y (buttonPos button) }
     setX x (Position (_, y)) = Position (x, y)
@@ -46,10 +98,10 @@ generateEmptyButtons = map (scaleRow buttonLinearSize) rowSetter
 createWithNumber :: Float -> (a -> Float -> a) -> a -> [a]
 createWithNumber maxNum setter base = map (setter base) [-maxNum / 2 .. maxNum / 2]
 
-initialGeneratorState :: Generator
-initialGeneratorState = Generator
-  { generatorButtons = generateEmptyButtons
-  }
+
+
+maximumMapSize :: Float
+maximumMapSize = 30
 
 buttonSize :: Dimensions
 buttonSize = Dimensions $ double . (/ maximumMapSize) . fromIntegral . (\x -> x - 100) . snd $ initialWindowDimensions
@@ -58,28 +110,69 @@ buttonSize = Dimensions $ double . (/ maximumMapSize) . fromIntegral . (\x -> x 
 buttonLinearSize :: Float
 buttonLinearSize = fst . unwrap $ buttonSize
 
+
+
 renderGenerator :: Generator -> Picture
 renderGenerator generator = Pictures
-                          $ map (draw) (concat . generatorButtons $ generator)
-                          ++ map (borders) (concat . generatorButtons $ generator)
-  where draw button = Color (stateColor . buttonState $ button)
-                    $ uncurry Translate (unwrap . buttonPos $ button)
-                    $ uncurry rectangleSolid (unwrap $ buttonSize)
+                          $ map draw (concat . generatorButtons $ generator)
+                          ++ map borders (concat . generatorButtons $ generator)
+                          ++ map number ( filter (\b -> buttonNum b /= 0 && correctState b)
+                                        . concat
+                                        . generatorButtons
+                                        $ generator)
+                          ++ map system systemButtons
+  where draw button = uncurry Translate (unwrap . buttonPos $ button)
+                    $ appearanceToPicture $ buttonAppearance (buttonState button) (buttonPos button)
         borders button = Color cyan
                        $ uncurry Translate (unwrap . buttonPos $ button)
                        $ uncurry rectangleWire (unwrap $ buttonSize)
+        number button = Color cyan
+                      $ uncurry Translate (unwrap . buttonPos $ button)
+                      $ Scale 0.1 0.1
+                      $ Text (show $ buttonNum button)
+        correctState button = let s = buttonState button
+                              in s == Door
+                              || s == ButtonObject
+        system button = trace "AAA" $ Pictures
+          [ Color black
+            $ uncurry Translate ((\(x, y) -> (x + 80, y)) . unwrap . systemButtonPosition $ button)
+            $ uncurry rectangleWire (160, 50) -- fixme
+          , Color black
+            $ uncurry Translate (unwrap . systemButtonPosition $ button)
+            $ Scale 0.15 0.15
+            $ Text (systemButtonText $ button)
+          ]
+
+
 
 handleGeneratorInput :: Event -> Generator -> Generator
-handleGeneratorInput (EventKey (MouseButton LeftButton) Up _ (x, y)) generator = generator
-  { generatorButtons = map (map click) $ generatorButtons generator
+handleGeneratorInput (EventKey (MouseButton LeftButton) Up (Modifiers {shift = Up, ctrl = Up, alt = Up}) (x, y)) generator
+  = generator
+  { generatorButtons = map (map (clickLeftOnTileButton (x, y))) $ generatorButtons generator
   }
-  where click button = if hit button then button { buttonState = nextState . buttonState $ button } else button
-        hit b = let (x', y') = unwrap $ buttonPos b
-                in x >= x' - buttonLinearSize / 2
-                && x <= x' + buttonLinearSize / 2
-                && y >= y' - buttonLinearSize / 2
-                && y <= y' + buttonLinearSize / 2
+handleGeneratorInput (EventKey (MouseButton LeftButton) Up (Modifiers {shift = Down, ctrl = Up, alt = Up}) (x, y)) generator
+  = generator
+  { generatorButtons = map (map (clickLeftOnTileButtonWithShift (x, y))) $ generatorButtons generator
+  }
 handleGeneratorInput _ generator = generator
 
+clickLeftOnTileButton :: (Float, Float) -> Button -> Button
+clickLeftOnTileButton (x, y) button = if hit (x, y) (unwrap . buttonPos $ button)
+                                      then button { buttonState = nextState . buttonState $ button }
+                                      else button
+
+clickLeftOnTileButtonWithShift :: (Float, Float) -> Button -> Button
+clickLeftOnTileButtonWithShift (x, y) button = if hit (x, y) (unwrap . buttonPos $ button)
+                                               then button { buttonNum = nextNum . buttonNum $ button }
+                                               else button
+
+hit :: (Float, Float) -> (Float, Float) -> Bool
+hit (x, y) (x', y') = x >= x' - buttonLinearSize / 2
+                   && x <= x' + buttonLinearSize / 2
+                   && y >= y' - buttonLinearSize / 2
+                   && y <= y' + buttonLinearSize / 2
+
+
+
 advanceGenerator :: Float -> Generator -> Generator
-advanceGenerator _ generator = generator
+advanceGenerator _ = id
